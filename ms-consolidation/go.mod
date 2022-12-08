8d3d74559b1c851543844d5d66cdd862bbf1efe1
Natanael <natanaelsilva.progjs@gmail.com>
2022-12-07 22:33:39 -0300

    refactor: rename folder for ms-consolidation

--- ms-consolidation/go.mod
+++ ms-consolidation/go.mod
@@ -1,3 +1,5 @@
 module github.com/NatanSiilva/cartola_fc_microservices
 
 go 1.19
+
+require github.com/google/uuid v1.3.0
---
+++ ms-consolidation/internal/domain/entity/match.go
@@ -0,0 +1,44 @@
+package entity
+
+import (
+	"strconv"
+	"time"
+)
+
+type MatchResults struct {
+	teamAScore int
+	teamBScore int
+}
+
+func NewMatchResults(teamAScore, teamBScore int) *MatchResults {
+	return &MatchResults{
+		teamAScore: teamAScore,
+		teamBScore: teamBScore,
+	}
+}
+
+func (m *MatchResults) GetResult() string {
+	return strconv.Itoa(m.teamAScore) + "-" + strconv.Itoa(m.teamBScore)
+}
+
+type Match struct {
+	ID      string
+	TeamA   *Team
+	TeamB   *Team
+	TeamAID string
+	TeamBID string
+	Date    time.Time
+	Status  string
+	Results MatchResults
+}
+
+func NewMatch(id string, teamA *Team, teamB *Team, date time.Time) *Match {
+	return &Match{
+		ID:      id,
+		TeamA:   teamA,
+		TeamB:   teamB,
+		TeamAID: teamA.ID,
+		TeamBID: teamB.ID,
+		Date:    date,
+	}
+}
